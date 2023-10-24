b52c895aff6e5a1ae08aea4a0e3c2bf416772e98
stevy.sung <stevy.sung@kakaocorp.com>
2023-10-24 22:39:57 +0900

    feat: learning kotlin hard

--- src/test/kotlin/study/PersonTest.kt
+++ src/test/kotlin/study/PersonTest.kt
@@ -4,11 +4,41 @@ import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 
 class PersonTest {
+
     @Test
-    fun `test test`() {
-        val actual = Person(name = "홍길동", 20, nickname = "홍")
-        assertThat(actual.name).isEqualTo("홍길동")
-        assertThat(actual.age).isEqualTo(20)
-        assertThat(actual.nickname).isEqualTo("홍")
+    fun `이름 붙인 생성자`() {
+        val person = Person(name = "홍길동", age = 27, nickname = "홍")
+        assertThat(person.name).isEqualTo("홍길동")
+        assertThat(person.age).isEqualTo(27)
+        assertThat(person.nickname).isEqualTo("홍")
+        person.nickname = "hello"
+        assertThat(person.nickname).isEqualTo("hello")
+    }
+
+    @Test
+    fun `널 타압`() {
+        val person = Person("홍길동", 27, null)
+        assertThat(person.nickname).isNull()
+    }
+
+    @Test
+    fun `기본 인자`() {
+        val person = Person("홍길동", 20)
+        assertThat(person.nickname).isEqualTo(person.name)
+    }
+
+    @Test
+    fun `데이터 클래스`() {
+        val person1 = Person("홍길동", 20)
+        val person2 = Person("홍길동", 20)
+        assertThat(person1).isEqualTo(person2)
+    }
+
+    @Test
+    fun copy() {
+        val person = Person("홍길동", 20)
+        assertThat(person.nickname).isEqualTo(person.name)
+        val copy = person.copy(nickname = "홍스")
+        assertThat(copy.nickname).isEqualTo("홍스")
     }
 }
